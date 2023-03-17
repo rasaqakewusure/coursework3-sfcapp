e360f805df01c550304aa3e8e4e53bb78ae1801f
rasaqakewusure <74925863+rasaqakewusure@users.noreply.github.com>
2023-03-17 09:21:21 +0000

    committing dist new version2

---
+++ serviceworker.js
@@ -0,0 +1,44 @@
+var cacheName = "lessons-v1";
+var cacheFiles = [
+"index.html",
+//"subjects.js",
+"foodtech.png",
+"history.png",
+"mathematics.svg",
+"music.png",
+"pen.jpg",
+"physicaleducation.png"
+];
+
+
+//self.addEventListener("install", function(e) {
+//console.log("[Service Worker] Install");
+//e.waitUntil(
+//caches.open(cacheName).then(function(cache) {
+//console.log("[Service Worker] Caching files");
+//return cache.addAll(cacheFiles);
+//})
+//);
+//});
+
+self.addEventListener("fetch", function(e) {
+    e.respondWith(
+    caches.match(e.request).then(function (cachedFile) {
+    //download the file if it is not in the cache
+    if (cachedFile) {
+    console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
+    return cachedFile;
+    } else {
+    return fetch(e.request).then(function (response) {
+    return caches.open(cacheName).then(function (cache) {
+    //add the new file to the cache
+    cache.put(e.request, response.clone());
+    console.log("[Service Worker] Resource fetched and saved in the cache for: " +
+    e.request.url);
+    return response;
+    });
+    });
+    }
+    })
+    );
+    });
